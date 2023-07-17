77eeeb54b59fa840f103955af6930c629b0dcf0f
Jorge Orta <36802074+Orta21@users.noreply.github.com>
2023-07-17 10:00:57 -0400

    fix(package): add useragent to be able to access webview
    Ref. metriport/metriport-internal#799

--- Sources/MetriportSDK/MetriportWidget.swift
+++ Sources/MetriportSDK/MetriportWidget.swift
@@ -31,7 +31,7 @@ public struct MetriportWidget: UIViewRepresentable, WebViewHandlerDelegate {
         providers: [String]? = nil,
         url: String? = nil) {
             let config = WKWebViewConfiguration()
-
+            config.applicationNameForUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1"
             let webView = WKWebView(frame: .zero, configuration: config)
 
             self.webView = webView
